572e54925493f411218a351d3bc3826ff058360f
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2025-09-17 11:43:55 +0000

    [LeetCode Sync] Runtime - 2 ms (18.98%), Memory - 42.4 MB (99.94%)

--- my-folder/0075-sort-colors/solution.java
+++ my-folder/0075-sort-colors/solution.java
@@ -1,19 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {        
-        int min_index;
+        int shift = Arrays.stream(nums).min().getAsInt();
+        int k = Arrays.stream(nums).max().getAsInt() - shift;
+        int[] counts = new int[k + 1];
+        
+        for (int n : nums) {
+            counts[n - shift] += 1;
+        }
+        
+        int startingIndex = 0;
+        for (int i = 0; i < k + 1; i++) {
+            int count = counts[i];
+            counts[i] = startingIndex;
+            startingIndex += count;
+        }
+        
+        int sorted[] = new int[nums.length];
+        for (int n : nums) {
+            sorted[counts[n - shift]] = n;
+            counts[n - shift] += 1;
+        }
         
         for (int i = 0; i < nums.length; i++) {
-            min_index = i;
-            
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] < nums[min_index]) {
-                    min_index = j;
-                }
-            }
-            
-            int temp = nums[min_index];
-            nums[min_index] = nums[i];
-            nums[i] = temp;
+            nums[i] = sorted[i];
         }
     }
 }
