800c1138bfe69185a1dd95550a069ff2a6d92d84
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-02-23 05:27:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 52.9 MB (98.33%)

---
+++ my-folder/0088-merge-sorted-array/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let index1 = m -1, index2 = n - 1, index3 = m + n - 1;
+
+    while (index1 >= 0 && index2 >= 0) {
+        if (nums2[index2] >= nums1[index1]) {
+            nums1[index3--] = nums2[index2--];
+        }
+        else {
+            nums1[index3--] = nums1[index1--];
+        }
+    }
+
+    while (index2 >= 0) {
+        nums1[index3--] = nums2[index2--];
+    }
+};
