13bce8809b1a3a7ed23203740ffe3abc83625a12
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-02-24 09:12:53 +0000

    [LeetCode Sync] Runtime - 1 ms (99.12%), Memory - 49 MB (49.47%)

--- my-folder/0001-two-sum/solution.cs
+++ my-folder/0001-two-sum/solution.cs
@@ -1,10 +1,13 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
+        var count = new Dictionary<int, int>();
         for (int i = 0; i < nums.Length; i++) {
-            int index = Array.IndexOf(nums, target - nums[i]);
-            if (index >= 0 && index != i) {
-                return [i, index];
+            int complement = target - nums[i];
+            if (count.TryGetValue(complement, out int index)) {
+                return new[] {i, index};
             }
+
+            count[nums[i]] = i;
         }
         return null;
     }
