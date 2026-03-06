1ca257b5a40b1877ea174d47dacfa6eb8388e890
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-06 23:42:33 +1100

    Implement memoization function in solution.ts for TS

---
+++ my-folder/2623-memoize/solution.ts
@@ -0,0 +1,30 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+    const cache = new Map();
+
+    return function(...args) {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const output = fn(...args);
+        cache.set(key, output);
+
+        return output;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
