2a4bba07b4b5eeee43e74a6149bb142e0e789ddc
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-10 20:10:45 +1100

    Use spread operator for function arguments in cancellable

--- my-folder/2715-timeout-cancellation/solution.ts
+++ my-folder/2715-timeout-cancellation/solution.ts
@@ -2,7 +2,7 @@ type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string
 type Fn = (...args: JSONValue[]) => void
 
 function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
-    const timerId = setTimeout(() => fn.apply(null, args), t);
+    const timerId = setTimeout(() => fn(...args), t);
 
     return () => clearTimeout(timerId);
 };
