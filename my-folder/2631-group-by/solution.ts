dca9b115eb53abd452dc7200324fdb47bbf8c301
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-15 14:30:32 +1100

    Create solution.ts

---
+++ my-folder/2631-group-by/solution.ts
@@ -0,0 +1,20 @@
+interface Array<T> {
+    groupBy(fn: (item: T) => string): Record<string, T[]>
+}
+
+
+Array.prototype.groupBy = function<T>(fn) {
+    const result: Record<string, T[]> = {};
+
+    for (const value of this) {
+        const key = fn(value);
+
+        (result[key] = result[key] || []).push(value);
+    }
+
+    return result;
+}
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
