1312a43a399e08c7c806cf63a03b7ac4fe8de309
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-15 14:12:33 +1100

    Create solution_reduce.js

---
+++ my-folder/2631-group-by/solution_reduce.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((acc, value) => {
+        const key = fn(value);
+        (acc[key] = acc[key] || []).push(value);
+
+        return acc;
+    }, {});
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
