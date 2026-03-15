9474b247c31c4d7e14b4e518789eabfd1a6b49c9
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-15 16:51:11 +1100

    Create solution_recursion.js

---
+++ my-folder/2625-flatten-deeply-nested-array/solution_recursion.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    let result = [];
+
+    const flatArray = (array, depth) => {
+        for (const value of array) {
+            if (Array.isArray(value) && depth > 0) {
+                flatArray(value, depth - 1);
+            }
+            else {
+                result.push(value);
+            }
+        }
+        return result;
+    }
+
+    return flatArray(arr, n);
+};
