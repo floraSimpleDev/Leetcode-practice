8d53aae07e5e6823dc2a681ecb414c8554276420
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-15 17:22:19 +1100

    Create solution_queue.ts

---
+++ my-folder/2625-flatten-deeply-nested-array/solution_queue.ts
@@ -0,0 +1,26 @@
+type MultiDimensionalArray = (number | MultiDimensionalArray)[];
+
+var flat = function (arr:  MultiDimensionalArray, n: number):  MultiDimensionalArray {
+    let isNested = true;
+    let depth = 0;
+    let queue;
+
+    while (isNested && depth < n) {
+        isNested = false;
+        queue = [];
+
+        for (const value of arr) {
+            if (Array.isArray(value)) {
+                queue.push(...value);
+                isNested = true;
+            }
+            else {
+                queue.push(value);
+            }
+        }
+        depth++;
+        arr = [...queue];
+    }
+
+    return arr;
+};
