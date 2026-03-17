7dae333b941b0ebab84a14b3eed4520e34c0a33d
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-17 13:45:54 +1100

    Create solution_chaining.js

---
+++ my-folder/2636-promise-pool/solution_chaining.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Promise<any>}
+ */
+var promisePool = async function(functions, n) {
+    const executeNext = () => functions[n++]?.().then(executeNext);
+
+    return await Promise.all(functions.slice(0, n).map(fn => fn().then(executeNext)));
+};
+
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
