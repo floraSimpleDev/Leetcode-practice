a279d062fbfa2a5c504c69d4149ba00ce7c0d97c
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-18 01:13:08 +1100

    Create solution_interval.js

---
+++ my-folder/2676-throttle/solution_interval.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var throttle = function(fn, t) {
+    let timer = null;
+    let event = null;
+
+    const timeoutFn = () => {
+        if (!event) {
+            clearInterval(timer);
+            timer = null;
+        } else {
+            fn(...event);
+            event = null;
+        }
+    }
+
+    return function(...args) {
+        if (timer) {
+            event = args;
+        } else {
+            fn(...args);
+            timer = setInterval(timeoutFn, t);
+        }
+    }
+};
+
+/**
+ * const throttled = throttle(console.log, 100);
+ * throttled("log"); // logged immediately.
+ * throttled("log"); // logged at t=100ms.
+ */
