5cabd14694cf9bf496d9ef638ea046278f60232a
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:43:04 +1100

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,23 @@
+// Last updated: 25/03/2026, 21:43:04
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (!nums?.length) return 0;
+    
+    const n = nums.length;
+    if (n === 1) return 1;
+
+    let index = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        if (nums[i] !== nums[i + 1]) {
+            nums[index++] = nums[i];
+        }
+    }
+
+    nums[index++] = nums[n - 1];
+
+    return index;
+};
