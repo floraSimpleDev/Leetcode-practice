93ec3e5c7ba9bb01f8abeebf8d543dcfaf167a2c
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:42:58 +1100

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.cs
@@ -0,0 +1,19 @@
+// Last updated: 25/03/2026, 21:42:57
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        for (int i = digits.Length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] carriedDigits = new int[digits.Length + 1];
+        carriedDigits[0] = 1;
+
+        return carriedDigits;
+    }
+}
