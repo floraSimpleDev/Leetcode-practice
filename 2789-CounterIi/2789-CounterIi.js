8472b5674729f7185f4c2de28448d907e4c1ee18
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:46 +1100

    Create 2789-CounterIi

---
+++ 2789-CounterIi/2789-CounterIi.js
@@ -0,0 +1,27 @@
+// Last updated: 25/03/2026, 21:41:46
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let result = init;
+    return {
+        increment: function() {
+            return ++result;
+        },
+        decrement: function() {
+            return --result;
+        },
+        reset: function() {
+            result = init;
+            return result;
+        },
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
