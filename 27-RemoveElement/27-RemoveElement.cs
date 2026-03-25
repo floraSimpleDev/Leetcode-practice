f175ee820a95a88789aa1319637fba156f4fb726
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:43:06 +1100

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.cs
@@ -0,0 +1,15 @@
+// Last updated: 25/03/2026, 21:43:05
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int index = 0;
+
+        foreach (int value in nums) {
+            if (value != val) {
+                nums[index] = value;
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
