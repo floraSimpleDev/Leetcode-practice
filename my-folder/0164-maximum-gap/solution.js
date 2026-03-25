187c1333eabf9ddb2bcd80499bad75f7577ee856
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 16:50:00 +1100

    Create solution.js

---
+++ my-folder/0164-maximum-gap/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function(nums) {
+    if (nums === null || nums.length < 2) {
+        return 0;
+    }
+
+    nums.sort((a, b) => a - b);
+
+    let maxGap = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        maxGap = Math.max(maxGap, nums[i + 1] - nums[i]);
+    }
+
+    return maxGap;
+};
