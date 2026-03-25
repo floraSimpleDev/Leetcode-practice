ef4276ea06b8a666538fbaf5359310341b2f694b
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:43:11 +1100

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.ts
@@ -0,0 +1,17 @@
+// Last updated: 25/03/2026, 21:43:11
+function longestCommonPrefix(strs: string[]): string {
+    if (!strs.length) return "";
+
+    strs.sort();
+
+    let first = strs[0];
+    let last = strs[strs.length - 1];
+    let i = 0;
+
+    while (i < first.length &&
+            i < last.length &&
+            first[i] === last[i]
+            ) i++;
+
+    return first.slice(0, i);
+};
