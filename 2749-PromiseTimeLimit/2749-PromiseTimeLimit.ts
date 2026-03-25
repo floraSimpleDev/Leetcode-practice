20063734e328040854ffe32a74ee75151448bf25
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:53 +1100

    Create 2749-PromiseTimeLimit

---
+++ 2749-PromiseTimeLimit/2749-PromiseTimeLimit.ts
@@ -0,0 +1,28 @@
+// Last updated: 25/03/2026, 21:41:52
+type Fn = (...params: any[]) => Promise<any>;
+
+function timeLimit(fn: Fn, t: number): Fn {
+    
+    return async function(...args) {
+        return new Promise(async (resolve, reject) => {
+            const timeout = setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+
+            try {
+                const result = await fn(...args);
+                resolve(result);
+            }
+            catch(error) {
+                reject(error);
+            }
+
+            clearTimeout(timeout);
+        });
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
