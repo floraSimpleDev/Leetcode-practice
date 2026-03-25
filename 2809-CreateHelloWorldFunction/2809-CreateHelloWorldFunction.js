43a5162f7a88ed02f5ca255490c049bb5a342502
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:33 +1100

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 25/03/2026, 21:41:33
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
