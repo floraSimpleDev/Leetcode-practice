57197e0d6da069fce2077eb69f646771f3734a12
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:59 +1100

    Create 2747-ApplyTransformOverEachElementInArray

---
+++ 2747-ApplyTransformOverEachElementInArray/2747-ApplyTransformOverEachElementInArray.js
@@ -0,0 +1,12 @@
+// Last updated: 25/03/2026, 21:41:59
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = fn(arr[i], i);
+    }
+    return arr;
+};
