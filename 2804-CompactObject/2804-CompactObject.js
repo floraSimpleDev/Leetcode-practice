cee14a30d972e5df8c755f67a4b3a359f83530b3
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:35 +1100

    Create 2804-CompactObject

---
+++ 2804-CompactObject/2804-CompactObject.js
@@ -0,0 +1,36 @@
+// Last updated: 25/03/2026, 21:41:34
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+    function dfs(obj) {
+        if (!obj) return false;
+        if (typeof obj !== "object") return obj;
+
+        if (Array.isArray(obj)) {
+            const subArray = [];
+
+            for (const value of obj) {
+                const subResult = dfs(value);
+
+                if (subResult) {
+                    subArray.push(subResult);
+                }
+            }
+            return subArray;
+        }
+
+        const subObj = {};
+        for (const key in obj) {
+            const subResult = dfs(obj[key]);
+            
+            if (subResult) {
+                subObj[key] = subResult;
+            }
+        }
+
+        return subObj;
+    }
+    return dfs(obj);
+};
