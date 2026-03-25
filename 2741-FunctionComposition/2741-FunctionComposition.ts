49c897eaf5e1c5af34f9f85d8d45a8416e51c8e8
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:42:06 +1100

    Create 2741-FunctionComposition

---
+++ 2741-FunctionComposition/2741-FunctionComposition.ts
@@ -0,0 +1,21 @@
+// Last updated: 25/03/2026, 21:42:06
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    let n = functions.length;
+    return function(x) {
+        if (n === 0) return x;
+
+        for (let i = n - 1; i >= 0; i--) {
+            const currentFn = functions[i];
+            x = currentFn(x);
+        }
+
+        return x;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
