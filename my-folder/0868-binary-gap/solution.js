ac0a72d5d88dcdd2addc4ba5511118b57f2c9a36
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 16:42:37 +1100

    Create solution.js

---
+++ my-folder/0868-binary-gap/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var binaryGap = function(n) {
+    const binaryN = n.toString(2);
+    let maxDistance = 0;
+    let lastIndex = -1;
+
+    for (let i = 0; i < binaryN.length; i++) {
+        if (binaryN[i] === '1') {
+            if (lastIndex !== -1) {
+                maxDistance = Math.max(maxDistance, i - lastIndex);
+            }
+
+            lastIndex = i;
+        }
+    }
+
+    return maxDistance;
+};
