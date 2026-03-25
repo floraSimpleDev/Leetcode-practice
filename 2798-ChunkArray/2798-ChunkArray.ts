507ceedc46f579c84877e445180996bd48d4deca
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:37 +1100

    Create 2798-ChunkArray

---
+++ 2798-ChunkArray/2798-ChunkArray.ts
@@ -0,0 +1,10 @@
+// Last updated: 25/03/2026, 21:41:37
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+    return Array.from({ 
+        length: Math.ceil(arr.length / size)
+        }, (_, index) => arr.slice(index * size, (index + 1) * size)
+    );
+};
