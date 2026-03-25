a167ee8995b589b3f07bcdd9a2a6c6a8f2d7e2b6
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:23 +1100

    Create 2859-AddTwoPromises

---
+++ 2859-AddTwoPromises/2859-AddTwoPromises.ts
@@ -0,0 +1,26 @@
+// Last updated: 25/03/2026, 21:41:23
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+    return new Promise<number>((resolve, reject) => {
+        let count = 2, result = 0;
+
+        [promise1, promise2].forEach(async promise => {
+            try {
+                const subResolve = await promise;
+                result += subResolve;
+                count--;
+
+                if (count === 0) resolve(result);
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    });
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
