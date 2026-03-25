3f1f8e86622d65aef8c904b9299c153584b96aaa
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:43:00 +1100

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.ts
@@ -0,0 +1,15 @@
+// Last updated: 25/03/2026, 21:43:00
+function searchInsert(nums: number[], target: number): number {
+    let left = 0, right = nums.length - 1, pivot;
+
+    while (left <= right) {
+        pivot = left + Math.floor((right - left) / 2);
+
+        if (nums[pivot] === target) return pivot;
+
+        if (nums[pivot] > target) right = pivot - 1;
+
+        else left = pivot + 1;
+    }
+    return left;
+};
