62f318416558cdebcc22c33124be514206cedc69
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:58 +1100

    Create 2745-ConvertObjectToJsonString

---
+++ 2745-ConvertObjectToJsonString/2745-ConvertObjectToJsonString.js
@@ -0,0 +1,29 @@
+// Last updated: 25/03/2026, 21:41:58
+/**
+ * @param {null|boolean|number|string|Array|Object} object
+ * @return {string}
+ */
+var jsonStringify = function(object) {
+    if (object === null) {
+        return 'null';
+    }
+
+    if (Array.isArray(object)) {
+        const items = object.map(item => jsonStringify(item));
+
+        return `[${items.join(',')}]`;
+    }
+
+    if (typeof object === 'object') {
+        const keys = Object.keys(object);
+        const pairs = keys.map(key => `"${key}":${jsonStringify(object[key])}`);
+
+        return `{${pairs.join(',')}}`;
+    }
+
+    if (typeof object === 'string') {
+        return `"${object}"`;
+    }
+
+    return String(object);
+};
