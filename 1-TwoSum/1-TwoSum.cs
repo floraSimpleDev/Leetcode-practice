8b55bcd7286c8c8f28fa714e2f9d0e21921af807
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:43:17 +1100

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cs
@@ -0,0 +1,15 @@
+// Last updated: 25/03/2026, 21:43:16
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var count = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+            if (count.TryGetValue(complement, out int index)) {
+                return new[] {i, index};
+            }
+
+            count[nums[i]] = i;
+        }
+        return null;
+    }
+}
