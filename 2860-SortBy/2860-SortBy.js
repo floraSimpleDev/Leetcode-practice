0ac12fa2c6c1b7e239576ed5107317b788fb4854
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:20 +1100

    Create 2860-SortBy

---
+++ 2860-SortBy/2860-SortBy.js
@@ -0,0 +1,9 @@
+// Last updated: 25/03/2026, 21:41:20
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a, b) => fn(a) - fn(b));
+};
