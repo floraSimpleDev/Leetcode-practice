144d77b9e2ded5d603e4fbaa0950f3caff061f93
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:42:22 +1100

    Create 2231-FindFirstPalindromicStringInTheArray

---
+++ 2231-FindFirstPalindromicStringInTheArray/2231-FindFirstPalindromicStringInTheArray.js
@@ -0,0 +1,23 @@
+// Last updated: 25/03/2026, 21:42:22
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+function isPalindrome(s) {
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+
+        left++, right--;
+    }
+    return true;
+}
+
+var firstPalindrome = function(words) {
+    const first = words.findIndex(word => isPalindrome(word));
+    if (first === -1) return '';
+    else return words[first];
+};
