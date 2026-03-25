2b8faad59d013a0fc3a0b774c474232046125e79
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:43:02 +1100

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.ts
@@ -0,0 +1,15 @@
+// Last updated: 25/03/2026, 21:43:02
+function plusOne(digits: number[]): number[] {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] === 9) digits[i] = 0;
+
+        else {
+            digits[i]++;
+
+            return digits;
+        }
+    }
+
+    digits.unshift(1);
+    return digits;
+};
