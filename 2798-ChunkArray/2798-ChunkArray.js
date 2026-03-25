a227eda3f7c3c37d45d1dd72a1212f40e7eb5791
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:40 +1100

    Create 2798-ChunkArray

---
+++ 2798-ChunkArray/2798-ChunkArray.js
@@ -0,0 +1,20 @@
+// Last updated: 25/03/2026, 21:41:40
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    return arr.reduce((result, value) => {
+        const chunk = result[result.length - 1];
+
+        if (!chunk || chunk.length === size) {
+            result.push([value]);
+        }
+        else {
+            chunk.push(value);
+        }
+
+        return result;
+    }, []);
+};
