a2da6f200b1072867431e099d723c37880bbc564
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:41:48 +1100

    Create 2762-CacheWithTimeLimit

---
+++ 2762-CacheWithTimeLimit/2762-CacheWithTimeLimit.js
@@ -0,0 +1,44 @@
+// Last updated: 25/03/2026, 21:41:48
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const existedValue = this.cache.get(key);
+    if (existedValue) {
+        clearTimeout(existedValue.timer);
+    }
+
+    const timer = setTimeout(() => this.cache.delete(key), duration);
+    this.cache.set(key, { value, timer });
+
+    return !!existedValue;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
