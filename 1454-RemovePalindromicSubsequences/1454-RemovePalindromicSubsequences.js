734fcd0f44d2dcf5a8bedda62ca739fc5a4fe47b
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:42:29 +1100

    Create 1454-RemovePalindromicSubsequences

---
+++ 1454-RemovePalindromicSubsequences/1454-RemovePalindromicSubsequences.js
@@ -0,0 +1,23 @@
+// Last updated: 25/03/2026, 21:42:29
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function isPalindrome(str) {
+    let left = 0, right = str.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) return false;
+
+        left++, right--;
+    }
+    return true;
+}
+
+var removePalindromeSub = function(s) {
+    if (!s) return 0;
+
+    if (isPalindrome(s)) return 1;
+
+    return 2;
+};
