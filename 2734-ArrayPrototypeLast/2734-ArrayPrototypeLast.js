679f615a908f6a5cfb5b78ccf41dd33229fbb065
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-25 21:42:10 +1100

    Create 2734-ArrayPrototypeLast

---
+++ 2734-ArrayPrototypeLast/2734-ArrayPrototypeLast.js
@@ -0,0 +1,13 @@
+// Last updated: 25/03/2026, 21:42:09
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    let val = this.pop()
+    return val !== undefined ? val : -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
