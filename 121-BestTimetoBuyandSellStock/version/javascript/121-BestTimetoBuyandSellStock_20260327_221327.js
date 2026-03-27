c96457a6b7726bdf9708e3beb8f51630f6b9bcd6
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-27 22:13:27 +1100

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/version/javascript/121-BestTimetoBuyandSellStock_20260327_221327.js
@@ -0,0 +1,20 @@
+// Last updated: 27/03/2026, 22:13:27
+1/**
+2 * @param {number[]} prices
+3 * @return {number}
+4 */
+5var maxProfit = function(prices) {
+6    let minPrice = Number.MAX_VALUE;
+7    let maxProfit = 0;
+8
+9    for (let price of prices) {
+10        if (price < minPrice) {
+11            minPrice = price;
+12        }
+13        else if (price - minPrice > maxProfit) {
+14            maxProfit = price - minPrice;
+15        }
+16    }
+17
+18    return maxProfit;
+19};
