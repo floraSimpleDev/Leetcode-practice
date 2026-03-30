1cf0f730957f8107e49ca46b57c36ab03b4ee40a
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-30 21:14:27 +1100

    Create 119-PascalsTriangleIi

---
+++ 119-PascalsTriangleIi/119-PascalsTriangleIi.js
@@ -0,0 +1,17 @@
+// Last updated: 30/03/2026, 21:14:26
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    let row = new Array(rowIndex + 1).fill(0);
+    row[0] = 1;
+
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = i; j > 0; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+    }
+
+    return row;
+};
