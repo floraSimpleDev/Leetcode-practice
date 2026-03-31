ee522fff89871fd2bc75b3a13cdea66a03e15238
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-03-31 23:29:31 +1100

    Create 841-ShortestDistanceToACharacter

---
+++ 841-ShortestDistanceToACharacter/841-ShortestDistanceToACharacter.js
@@ -0,0 +1,22 @@
+// Last updated: 31/03/2026, 23:29:31
+/**
+ * @param {string} s
+ * @param {character} c
+ * @return {number[]}
+ */
+var shortestToChar = function(s, c) {
+    let prev = s.indexOf(c);
+    let next = prev;
+    const distance = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === c) {
+            prev = i;
+            next = s.indexOf(c, prev + 1);
+        }
+
+        distance.push(Math.min(Math.abs(prev - i), Math.abs(next - i)));
+    }
+
+    return distance;
+};
