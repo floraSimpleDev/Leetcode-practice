b21bb57f24d37e1531daf1ac727f3e4ca6806b09
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-04-07 22:44:35 +1000

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/version/javascript/217-ContainsDuplicate_20260407_224435.js
@@ -0,0 +1,14 @@
+// Last updated: 07/04/2026, 22:44:35
+1/**
+2 * @param {number[]} nums
+3 * @return {boolean}
+4 */
+5var containsDuplicate = function(nums) {
+6    let numbers = new Set();
+7
+8    for (let number of nums) {
+9        if (numbers.has(number)) return true;
+10        numbers.add(number);
+11    }
+12    return false;
+13};
