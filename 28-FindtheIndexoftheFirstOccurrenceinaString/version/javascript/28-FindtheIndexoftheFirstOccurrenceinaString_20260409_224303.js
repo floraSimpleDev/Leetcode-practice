1e291481186d3c8814595cf22d7af5f412c0131b
Flora Lee <159777651+floraSimpleDev@users.noreply.github.com>
2026-04-09 22:43:04 +1000

    Create 28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/version/javascript/28-FindtheIndexoftheFirstOccurrenceinaString_20260409_224303.js
@@ -0,0 +1,9 @@
+// Last updated: 09/04/2026, 22:43:03
+1/**
+2 * @param {string} haystack
+3 * @param {string} needle
+4 * @return {number}
+5 */
+6var strStr = function(haystack, needle) {
+7    return haystack.indexOf(needle);
+8};
